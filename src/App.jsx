7dbcbd5194d54624417b44e2a99c608d11edf102
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-07-29 16:37:28 -0400

    gato

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,98 @@
-import { useState } from "react";
-const WINER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-const GRID = Array.from(Array(9).keys());
+import { useState } from 'react'
+import confetti from 'canvas-confetti'
 
-const TURNS = {
-  "X": "❌",
-  "O": "⭕",
-};
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js'
 
-function App() {
-  const [player, setPlayer] = useState(() => {
-    const playerFromStorage  = window.localStorage.getItem("player");
-      return playerFromStorage ? JSON.parse(playerFromStorage) : TURNS.X;
-  });
-  const [plays, setPlays] = useState(() =>{
-    const playsFromStorage = window.localStorage.getItem("plays");
-    
-    return playsFromStorage ? new Map(JSON.parse(playsFromStorage)) : new Map(); 
-  });
-  const [points, setPoints] = useState(() => {
-    const pointsFromStorage = window.localStorage.getItem("points");
-    return pointsFromStorage ? JSON.parse(pointsFromStorage) : { player1: 0, player2: 0 };
+function App () {
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  })
+
+  const [winner, setWinner] = useState(null)
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
   }
-  );
 
-  function handleClick(cell) {
-    if (plays.has(cell)) return;
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
 
-    const draf = new Map(plays);
-    draf.set(cell, player);
-
-    const winner = WINER_COMBOS.find((combo) => {
-      return combo.every((cell) => draf.get(cell) === player);
-    });
-
-    if (!winner && draf.size === 9) {
-      setPlays(new Map());
-      return;
-    }
-
-    setPlays(draf);
-    
-    setPlayer((prevPlayer) => (prevPlayer === TURNS.X ? TURNS.O : TURNS.X));
-
-    window.localStorage.setItem("plays", JSON.stringify([...plays]));
-    window.localStorage.setItem("player", JSON.stringify(player));
-    window.localStorage.setItem("points", JSON.stringify(points));
-    if (winner) {
-      if (player === TURNS.X) {
-        setPoints({ player1: points.player1 + 1, player2: points.player2 });
-      } else {
-        setPoints({ player1: points.player1, player2: points.player2 + 1 });
-      }
-      setPlays(new Map());
-      return;
-    }
+    resetGameStorage()
   }
-  function reset() {
-    setPlays(new Map());
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
+    const newWinner = checkWinnerFrom(newBoard)
+    if (newWinner) {
+      confetti({
+        angle: randomInRange(55, 125),
+        spread: randomInRange(50, 70),
+        particleCount: randomInRange(50, 100),
+        origin: { y: 0.6 }
+      })
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
   }
 
   return (
-    <main className="flex flex-col gap-4">
-      <h1 className="text-center text-3xl font-bold text-stone-300">Gato 😺</h1>
-      <header className="flex justify-between items-center text-center text-xl font-bold text-stone-300">
-        <span className={`${TURNS.X === player ? "bg-stone-500 rounded-lg p-2" : ""}`}>❌ {points.player1}</span>
-        <span className={`${TURNS.O === player ? "bg-stone-500 rounded-lg p-2" : ""}`}>⭕ {points.player2}</span>
-      </header>
-      <section className="grid grid-cols-3 gap-4 p-4 rounded-lg bg-stone-900 w-full h-full">
-        {GRID.map((i) => (
-          <button
-            className="rounded-lg w-20 h-20 aspect-square border border-transparent border-solid text-center font-medium text-3xl bg-stone-500 cursor-pointer transition-colors hover:border-emerald-100"
-            key={i}
-            onClick={() => handleClick(i)}
-          >
-            {plays.get(i)}
-          </button>
-        ))}
+    <main className='board'>
+      <h1 className="text-3xl">Gato 😺</h1>
+      <button onClick={resetGame}>Reiniciar juego</button>
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
       </section>
-      <footer className="flex justify-center items-center text-center text-xl font-bold text-stone-300">
-        <button
-          className="bg-stone-900 py-2 px-4 rounded-lg"
-          onClick={() => reset()}
-        >
-          Reiniciar partida
-        </button>
-      </footer>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
